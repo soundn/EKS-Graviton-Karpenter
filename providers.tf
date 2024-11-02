c359ce2511bd1925ba03cf9d0344c2ab7ee93396
soundn <soundnwankwo@gmail.com>
2024-11-02 13:26:19 +0100

    fixed

--- providers.tf
+++ providers.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,13 +11,8 @@ terraform {
     }
     
     kubectl = {
-      source  = "gavinbunney/kubectl"  # Fixed the source from hashicorp to gavinbunney
-      version = ">= 1.14.0"
-    }
-    
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.5.1"
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
     }
   }
 }
@@ -31,7 +24,6 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -42,24 +34,9 @@ provider "kubernetes" {
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
-
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    }
   }
 }
