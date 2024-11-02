fa415b6e4e3493d18106abbefc90efa495153d4a
soundn <soundnwankwo@gmail.com>
2024-11-02 13:53:30 +0100

    fix

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,7 +1,19 @@
 output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.main.endpoint
 }
 
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_id" {
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster"
+  value       = aws_eks_cluster.main.version
 }
